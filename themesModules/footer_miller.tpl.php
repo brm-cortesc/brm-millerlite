b94ccc92649c7f27a09dae18e0dfc7ed68d5e93b
Camilo Rivera <camilo.rivera@brm.com.co>
2015-12-24 13:14:03 -0500

    css

--- themesModules/footer_miller.tpl.php
+++ themesModules/footer_miller.tpl.php
@@ -1,15 +1,15 @@
 <?php global $base_url; ?>
 
+
+<div class="foot">
+<p class="log-imt"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/its-miller-time.png'); ?>" alt="Miller Lite" class="pd-log"></p>  
+</div>
+
 <footer class="foot">
-  <section class="container">
-    <div class="row">
-      <!-- logo -->
-        <article class="col-lg-1 col-md-2 col-sm-12 col-xs-12">
-            <p class="log-imt"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/its-miller-time.png'); ?>" alt="Miller Lite" class="pd-log"></p>
-        </article>
-      <!-- lista -->
-          <article class="col-lg-8 col-md-10 col-sm-12 col-xs-12 ft-mob">
-           <?php $menu = menu_navigation_links('menu-footer-menu');
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <p class="cent-lst"> 
+          <?php $menu = menu_navigation_links('menu-footer-menu');
            foreach ($menu as $key => $value) {
             $aux = $key;
             $onlyconsonants = str_replace(" ", "_", $value["title"]);
@@ -21,23 +21,23 @@
            }
             print theme('links', array('links' => $menu,'attributes' => array('class' => array('nav navbar-nav list-bl'))));
         ?>
-       </article>
-      <!-- politicas de alcohol -->
-
-      <article class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ft-mob">
-      <div class="row">
-        <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
-          <p class="leg-txt">Prohíbese el expendio de bebidas embriagantes a menores de edad. El exceso de alcohol es perjudicial para la salud. </p>
-        </div>
-        <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
-          <p class="c-lg"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/talking-alcohol-ml.png'); ?>" alt="hablemos de alcohol" class="pd-log"></p>
-        </div>
-      </div>
-      </div>
-
-
-
-
+      </p>
     </div>
-  </section>  
+  </div>
 </footer>
+
+
+<div class="foot">
+  <div class="row">
+    
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <p class="leg-txt">Prohíbese el expendio de bebidas embriagantes a menores de edad. El exceso de alcohol es perjudicial para la salud. </p>
+    </div>
+    
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+          <p class="c-lg"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/talking-alcohol-ml.png'); ?>" alt="hablemos de alcohol" class="pd-log"></p>
+    </div>
+
+    
+  </div>
+</div>
