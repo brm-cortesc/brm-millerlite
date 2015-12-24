4662516f2e3a40bdd6217fcd3af88b1689883df9
Camilo Rivera <camilo.rivera@brm.com.co>
2015-12-24 11:01:14 -0500

    css

--- themesModules/footer_miller.tpl.php
+++ themesModules/footer_miller.tpl.php
@@ -1,9 +1,9 @@
 <?php global $base_url; ?>
     <footer class="container-fluid navbar navbar-default navbar-fixed-bottom del-ft">
-    <article class="col-lg-1 col-md-2 col-sm-12 col-xs-12">
+    <article class="col-lg-1 col-md-1 col-sm-12 col-xs-12">
       <p class="log-imt"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/its-miller-time.png'); ?>" alt="Miller Lite" class="pd-log"></p>
     </article>
-    <article class="col-lg-8 col-md-10 col-sm-12 col-xs-12 ft-mob">
+    <article class="col-lg-7 col-md-7 col-sm-12 col-xs-12 ft-mob">
       <?php $menu = menu_navigation_links('menu-footer-menu');
            foreach ($menu as $key => $value) {
             $aux = $key;
@@ -17,12 +17,12 @@
             print theme('links', array('links' => $menu,'attributes' => array('class' => array('nav navbar-nav list-bl'))));
         ?>
     </article>
-    <article class="col-lg-3 col-md-10 col-sm-12 col-xs-12">
+    <article class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
       <div class="row">
-        <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
           <p class="leg-txt">Prohíbese el expendio de bebidas embriagantes a menores de edad. El exceso de alcohol es perjudicial para la salud. </p>
         </div>
-        <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
           <p class="c-lg"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/talking-alcohol-ml.png'); ?>" alt="hablemos de alcohol" class="pd-log"></p>
         </div>
       </div>
