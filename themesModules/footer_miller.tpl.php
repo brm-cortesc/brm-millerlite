858ca869a571ff15ba8230f26c3f550ba0fa96d2
Camilo Rivera <camilo.rivera@brm.com.co>
2015-12-24 12:52:04 -0500

    css

--- themesModules/footer_miller.tpl.php
+++ themesModules/footer_miller.tpl.php
@@ -1,10 +1,15 @@
 <?php global $base_url; ?>
-    <footer class="container-fluid navbar navbar-default navbar-fixed-bottom del-ft">
-    <article class="col-lg-1 col-md-2 col-sm-12 col-xs-12">
-      <p class="log-imt"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/its-miller-time.png'); ?>" alt="Miller Lite" class="pd-log"></p>
-    </article>
-    <article class="col-lg-8 col-md-10 col-sm-12 col-xs-12 ft-mob">
-      <?php $menu = menu_navigation_links('menu-footer-menu');
+
+<footer class="foot">
+  <section class="container">
+    <div class="row">
+      <!-- logo -->
+        <article class="col-lg-1 col-md-2 col-sm-12 col-xs-12">
+            <p class="log-imt"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/its-miller-time.png'); ?>" alt="Miller Lite" class="pd-log"></p>
+        </article>
+      <!-- lista -->
+          <article class="col-lg-8 col-md-10 col-sm-12 col-xs-12 ft-mob">
+           <?php $menu = menu_navigation_links('menu-footer-menu');
            foreach ($menu as $key => $value) {
             $aux = $key;
             $onlyconsonants = str_replace(" ", "_", $value["title"]);
@@ -16,8 +21,10 @@
            }
             print theme('links', array('links' => $menu,'attributes' => array('class' => array('nav navbar-nav list-bl'))));
         ?>
-    </article>
-    <article class="col-lg-3 col-md-10 col-sm-12 col-xs-12">
+       </article>
+      <!-- politicas de alcohol -->
+
+      <article class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ft-mob">
       <div class="row">
         <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
           <p class="leg-txt">Prohíbese el expendio de bebidas embriagantes a menores de edad. El exceso de alcohol es perjudicial para la salud. </p>
@@ -26,5 +33,11 @@
           <p class="c-lg"><img src="<?php print_r($base_url.'/sites/all/themes/millerLiteColTheme/images/talking-alcohol-ml.png'); ?>" alt="hablemos de alcohol" class="pd-log"></p>
         </div>
       </div>
-    </article>
-  </footer>
+      </div>
+
+
+
+
+    </div>
+  </section>  
+</footer>
